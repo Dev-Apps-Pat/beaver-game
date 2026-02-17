17215388406045a661e287bafc792364dfee05b6
finance-control-dev <patrickcopytec@gmail.com>
2026-02-17 03:56:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'beaver-game-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './normal.png',
+    './hole.png',
+    './yes.png',
+    './no.png',
+    './over.png',
+    './background.png',
+    './beaver_main.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
